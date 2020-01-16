d9efce0ebfb5c95c7115747e37a156e0854e807a
sahildiwan92 <isahildiwan1992@gmail.com>
2020-01-17 01:05:22 +0530

    Feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,28 @@ import { AppComponent } from './app.component';
 import { BikeService } from './services/bike.service';
 import { HttpClientModule } from '@angular/common/http';
 import { AdminComponent } from './components/admin/admin.component';
+import { HomeComponent } from './components/home/home.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ViewRegistrationComponent } from './components/view-registration/view-registration.component';
+import { CallbackComponent } from './components/callback/callback.component';
+import { AuthService } from './services/auth.service';
+import { AuthGuard } from './services/auth.guard';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdminComponent
+    AdminComponent,
+    HomeComponent,
+    ViewRegistrationComponent,
+    CallbackComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
-  providers: [BikeService],
+  providers: [BikeService,AuthService,AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/services/bike.service.ts
+++ src/app/services/bike.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
+
+const httpOptions = {
+  headers: new HttpHeaders({'content-Type': 'application/json'})
+};
+
 @Injectable({
   providedIn: 'root'
 })
@@ -9,6 +14,21 @@ export class BikeService {
   constructor(private http: HttpClient) { }
 
   getBikes(){
-    return this.http.get('/server/api/v1/bikes');
+    let token = localStorage.getItem('access_token');
+    return this.http.get('/server/api/v1/bikes',
+    {headers: new HttpHeaders().set('Authorization','Bearer'+token)}
+  );
+  }
+  
+  getBike(id:number){
+    let token = localStorage.getItem('access_token');
+    return this.http.get('/server/api/v1/bikes/'+id,
+    {headers: new HttpHeaders().set('Authorization','Bearer'+token)}
+  );
+  }
+
+  createBikeRegistration(bike){
+    let body = JSON.stringify(bike);
+    return this.http.post('/server/api/v1/bikes',body,httpOptions);
   }
 }
